95be572f48ac57531e6f1803717dc0d7646edca7
SomeRandomGuy45 <dragonlionmusic@gmail.com>
2024-08-29 18:58:56 -0500

    fixed desktop

--- CreateInstaller.sh
+++ CreateInstaller.sh
@@ -1,12 +1,12 @@
-mkdir -p ~/Desktop/InstallerPackage/Scripts
-cp ~/Desktop/Installer/Install.sh ~/Desktop/InstallerPackage/Scripts/postinstall
-mkdir ~/Desktop/InstallerPackage/Root
-pkgbuild --root ~/Desktop/InstallerPackage/Root --scripts ~/Desktop/InstallerPackage/Scripts --identifier com.macbloxinstaller --version 1.0 --install-location /Applications ~/Desktop/Installer/Installer.pkg
-rm -rf ~/Desktop/InstallerPackage
-mkdir -p ~/Desktop/InstallerPackage/Scripts
-cp ~/Desktop/Installer/Install_Testing.sh ~/Desktop/InstallerPackage/Scripts/postinstall
-mkdir ~/Desktop/InstallerPackage/Root
-pkgbuild --root ~/Desktop/InstallerPackage/Root --scripts ~/Desktop/InstallerPackage/Scripts --identifier com.macbloxinstaller --version 1.0 --install-location /Applications ~/Desktop/Installer/Installer_Testing.pkg
-rm -rf ~/Desktop/InstallerPackage
+mkdir -p InstallerPackage/Scripts
+cp Installer/Install.sh InstallerPackage/Scripts/postinstall
+mkdir InstallerPackage/Root
+pkgbuild --root InstallerPackage/Root --scripts InstallerPackage/Scripts --identifier com.macbloxinstaller --version 1.0 --install-location /Applications Installer/Installer.pkg
+rm -rf InstallerPackage
+mkdir -p InstallerPackage/Scripts
+cp Installer/Install_Testing.sh InstallerPackage/Scripts/postinstall
+mkdir InstallerPackage/Root
+pkgbuild --root InstallerPackage/Root --scripts InstallerPackage/Scripts --identifier com.macbloxinstaller --version 1.0 --install-location /Applications Installer/Installer_Testing.pkg
+rm -rf InstallerPackage
 rm -rf Macblox.dmg
 appdmg installer.json Macblox.dmg
