2a38a8a53361764b1a207b1466358bc8574795b8
SomeRandomGuy45 <dragonlionmusic@gmail.com>
2024-08-29 19:03:30 -0500

    AHHHH

--- CreateInstaller.sh
+++ CreateInstaller.sh
@@ -1,12 +1,12 @@
 mkdir -p InstallerPackage/Scripts
-cp Installer/Install.sh InstallerPackage/Scripts/postinstall
+cp Install.sh InstallerPackage/Scripts/postinstall
 mkdir InstallerPackage/Root
-pkgbuild --root InstallerPackage/Root --scripts InstallerPackage/Scripts --identifier com.macbloxinstaller --version 1.0 --install-location /Applications Installer/Installer.pkg
+pkgbuild --root InstallerPackage/Root --scripts InstallerPackage/Scripts --identifier com.macbloxinstaller --version 1.0 --install-location /Applications Installer.pkg
 rm -rf InstallerPackage
 mkdir -p InstallerPackage/Scripts
-cp Installer/Install_Testing.sh InstallerPackage/Scripts/postinstall
+cp Install_Testing.sh InstallerPackage/Scripts/postinstall
 mkdir InstallerPackage/Root
-pkgbuild --root InstallerPackage/Root --scripts InstallerPackage/Scripts --identifier com.macbloxinstaller --version 1.0 --install-location /Applications Installer/Installer_Testing.pkg
+pkgbuild --root InstallerPackage/Root --scripts InstallerPackage/Scripts --identifier com.macbloxinstaller --version 1.0 --install-location /Applications Installer_Testing.pkg
 rm -rf InstallerPackage
 rm -rf Macblox.dmg
 appdmg installer.json Macblox.dmg
