685c7530bfcde2d50b475d63c3427e76c2e54d20
SomeRandomGuy45 <143886716+SomeRandomGuy45@users.noreply.github.com>
2024-09-23 19:06:29 -0500

    ok it should work now!

--- Install.sh
+++ Install.sh
@@ -19,6 +19,7 @@ killall Macblox
 # soon add a file
 # cuz we are just assuming the version to be the latest
 latest_version=$(curl -s "https://api.github.com/repos/SomeRandomGuy45/MacBlox/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+current_user=$(whoami)
 
 ARCH=$(uname -m)
 
@@ -49,28 +50,26 @@ EOF
 
 PASSWORD=$(osascript -e 'Tell application "System Events" to display dialog "Enter your password:" default answer "" with hidden answer buttons {"OK"} default button "OK"' -e 'text returned of result')
 #create_loading_dialog
-cd ~
-echo "$PASSWORD" | sudo -S rm -rf Macblox_Build
-echo "$PASSWORD" | sudo -S rm -rf Macblox_Build.zip
+echo "$PASSWORD" | sudo -S rm -rf /Users/$current_user/Macblox_Build
+echo "$PASSWORD" | sudo -S rm -rf /Users/$current_user/Macblox_Build.zip
 echo "[INFO] Building MacBlox"
 # Download the repository as a ZIP file
-curl -L -o Macblox_Build.zip https://github.com/SomeRandomGuy45/Macblox_Build/archive/refs/heads/main.zip
+curl -L -o /Users/$current_user/Macblox_Build.zip https://github.com/SomeRandomGuy45/Macblox_Build/archive/refs/heads/main.zip
 # Unzip the downloaded ZIP file
-unzip Macblox_Build.zip
+unzip /Users/$current_user/Macblox_Build.zip
 # Move into the extracted directory (it'll be named "Macblox_Build-main")
-cd Macblox_Build-main
-cd "Macblox_${TYPE}"
-echo "$PASSWORD" | sudo -S chmod +x "Play.app/Contents/MacOS/play"
-echo "$PASSWORD" | sudo -S chmod +x "Play.app/Contents/Resources/Discord"
-echo "$PASSWORD" | sudo -S chmod +x "Play.app/Contents/MacOS/Bootstrap.app/Contents/MacOS/bootstrap"
-echo "$PASSWORD" | sudo -S chmod +x "Play.app/Contents/MacOS/Bootstrap.app/Contents/Resources/helper.sh"
-echo "$PASSWORD" | sudo -S chmod +x "Play.app/Contents/MacOS/GameWatcher.app/Contents/MacOS/GameWatcher"
-echo "$PASSWORD" | sudo -S chmod +x "Macblox.app/Contents/MacOS/Macblox"
-echo "$PASSWORD" | sudo -S chmod +x "Open Roblox.app/Contents/MacOS/openRoblox"
-cd ..
-mkdir -p "~/Library/Application Support/Macblox_Installer_Data"
 
-cat <<EOF > ~/Library/Application\ Support/Macblox_Installer_Data/config.json
+echo "$PASSWORD" | sudo -S chmod +x "/Users/$current_user/Macblox_Build-main/Macblox_${TYPE}Play.app/Contents/MacOS/play"
+echo "$PASSWORD" | sudo -S chmod +x "/Users/$current_user/Macblox_Build-main/Macblox_${TYPE}Play.app/Contents/Resources/Discord"
+echo "$PASSWORD" | sudo -S chmod +x "/Users/$current_user/Macblox_Build-main/Macblox_${TYPE}Play.app/Contents/MacOS/Bootstrap.app/Contents/MacOS/bootstrap"
+echo "$PASSWORD" | sudo -S chmod +x "/Users/$current_user/Macblox_Build-main/Macblox_${TYPE}Play.app/Contents/MacOS/Bootstrap.app/Contents/Resources/helper.sh"
+echo "$PASSWORD" | sudo -S chmod +x "/Users/$current_user/Macblox_Build-main/Macblox_${TYPE}Play.app/Contents/MacOS/GameWatcher.app/Contents/MacOS/GameWatcher"
+echo "$PASSWORD" | sudo -S chmod +x "/Users/$current_user/Macblox_Build-main/Macblox_${TYPE}Macblox.app/Contents/MacOS/Macblox"
+echo "$PASSWORD" | sudo -S chmod +x "/Users/$current_user/Macblox_Build-main/Macblox_${TYPE}Open Roblox.app/Contents/MacOS/openRoblox"
+cd ..
+mkdir -p "/Users/$current_user/Library/Application Support/Macblox_Installer_Data"
+
+cat <<EOF > /Users/$current_user/Library/Application\ Support/Macblox_Installer_Data/config.json
 {
    "version" : "${latest_version}",
    "branch" : "main"
@@ -80,6 +79,6 @@ EOF
 echo "[INFO] Finshed building MacBlox"
 echo "$PASSWORD" | sudo -S mkdir /Applications/Macblox
 #should copy the items not the folder it self
-echo "$PASSWORD" | sudo -S mv "Macblox_${TYPE}"/* /Applications/Macblox
+echo "$PASSWORD" | sudo -S mv "/Users/$current_user/Macblox_Build-main/Macblox_${TYPE}"/* /Applications/Macblox
 sleep 1
 #close_loading_dialog
